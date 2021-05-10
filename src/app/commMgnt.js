ff40e0b7f8fd3a2993ee8faf58b9d73907c8fcad
patrykgitt <patrykbrn@gmail.com>
2021-05-10 21:35:11 +0200

    commMngr test design

--- src/app/commMgnt.js
+++ src/app/commMgnt.js
@@ -3,13 +3,14 @@ export class PlcCommMngr {
     const maxRegPerReq = 32;
     const startReg = o;
     const noOfRegsPerString = 16;
-    const noOfStrins = 16;
+    const noOfStrings = 16;
     var plcRegs;
+    stringsRegs(startReg,noOfRegsPerString,noOfStrings)
   }
   stringsRegs(startReg, noOfRegsPerSting, noOfStrings) {
     noOfRegs = noOfRegsPerSting * noOfStrings;
     for (let i = 0; i < noOfRegs; i++) {
-      this.plcRegs.append();
+      this.plcRegs.append({device:"test";type:"int"});
     }
   }
 }
