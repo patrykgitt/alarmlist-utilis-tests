209a85c92f65cf478df2a6ce5fdafc5d701b11b7
patrykgitt <patrykbrn@gmail.com>
2021-05-10 16:04:15 +0200

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,8 @@
+import { Human } from './test2';
 describe('Testing tests', () => {
+  var calc = new Human(18);
+  it('should succeed', () => expect(calc.age2()).toEqual(18));
+
   it('should succeed', () => expect(true).toEqual(true));
   it('should fail', () => expect(true).toEqual(false));
 });
