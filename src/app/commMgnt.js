97af510a696e0da8438d240d30b42470df3957c5
patrykgitt <patrykbrn@gmail.com>
2021-05-10 21:06:28 +0200

    commMngmnt

---
+++ src/app/commMgnt.js
@@ -0,0 +1,15 @@
+export class PlcCommMngr {
+  constructor() {
+    const maxRegPerReq = 32;
+    const startReg = o;
+    const noOfRegsPerString = 16;
+    const noOfStrins = 16;
+    var plcRegs;
+  }
+  stringsRegs(startReg, noOfRegsPerSting, noOfStrings) {
+    noOfRegs = noOfRegsPerSting * noOfStrings;
+    for (let i = 0; i < noOfRegs; i++) {
+      this.plcRegs.append();
+    }
+  }
+}
