efc6c0e50548f61af5207bfc3a5c8eb78b8c81fc
patrykgitt <patrykbrn@gmail.com>
2021-05-11 08:41:41 +0200

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,14 @@
-import { Human } from './test2';
+//import { Human } from './test2';
+///describe('Testing tests', () => {
+//  var calc = new Human(18);
+//  it('should succeed', () => expect(calc.age2()).toEqual(18));
+//
+//  it('should succeed', () => expect(true).toEqual(true));
+//  it('should fail', () => expect(true).toEqual(false));
+//});
+import { PlcCommMgnt } from './commMgnt';
 describe('Testing tests', () => {
-  var calc = new Human(18);
+  var commMgnt = new PlcCommMgnt();
   it('should succeed', () => expect(calc.age2()).toEqual(18));
 
   it('should succeed', () => expect(true).toEqual(true));
--- src/app/commMgnt.js
+++ src/app/commMgnt.js
@@ -1,16 +1,16 @@
-export class PlcCommMngr {
+export class PlcCommMgnt {
   constructor() {
     const maxRegPerReq = 32;
     const startReg = o;
     const noOfRegsPerString = 16;
     const noOfStrings = 16;
     var plcRegs;
-    stringsRegs(startReg,noOfRegsPerString,noOfStrings)
+    stringsRegs(startReg, noOfRegsPerString, noOfStrings);
   }
   stringsRegs(startReg, noOfRegsPerSting, noOfStrings) {
     noOfRegs = noOfRegsPerSting * noOfStrings;
     for (let i = 0; i < noOfRegs; i++) {
-      this.plcRegs.append({device:"test";type:"int"});
+      this.plcRegs.append({ device: 'test', type: 'int' });
     }
   }
 }
